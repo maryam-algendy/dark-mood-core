429977da7d6a7a2ca3e3e07cfd7cd33154f148e7
maryam-algendy <maryamalgendy@gmail.com>
2020-10-27 16:08:17 +0200

    dark-mode

---
+++ Desktop/dark-mood/js/plugin.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark");
+    
+    var img=document.getElementById("img").src;
+    if(img=="http://127.0.0.1:5500/dark.png"){
+        document.getElementById("img").src="http://127.0.0.1:5500/light.png"
+        localStorage.setItem("theme","dark")
+    }
+    else{
+        document.getElementById("img").src="http://127.0.0.1:5500/dark.png"
+        localStorage.setItem("theme","light")
+    }
+}
+window.onload=checkTheme();
+function checkTheme(){
+    const theme=localStorage.getItem("theme");
+    console.log(theme);
+    if(theme==="dark"){
+        myFunction();
+    }
+    
+}
